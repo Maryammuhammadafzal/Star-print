73a6104e9c9502942a480b95c5c97ed502f760a1
maryammuhammadafzal <natiqnagori7@gmail.com>
2024-06-11 22:39:57 +0500

    print-star-with-nested-loop

---
+++ app.js
@@ -0,0 +1,39 @@
+
+// document.write('<h1> Printing Stars </h1>')
+// // Printing STars
+
+// for (i = 0; i <= 7; i++){
+//     for(j = 1; j <= i; j++){
+//         document.write('<b> * </b>');
+        
+//     }
+//     document.write('<br>');
+// }
+
+// document.write('<br>');
+// // Reverse Stars
+
+// for (i = 7; i >= 0; i--){
+//     for(j = 1; j <= i; j++){
+//         document.write('<b> * </b>');
+        
+//     }
+//     document.write('<br>');
+// }
+
+// document.write('<br>');
+
+// Name Printing
+
+for (i = 0; i <= 0; i++){
+    for(j = 1; j <= 10; j++){
+        document.write('<b> * </b>' + '&nbsp;' );
+        if(j == 4){
+
+            continue;
+        }
+    }
+    document.write('<br>' + ' ' );
+}
+
+document.write('<br>');
